3edca06ae3a86ee2a498d6711fcca12d66430fb5
Romain <romain@cantoo.co>
2020-12-29 17:39:47 +0100

    Updates and cleanup

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,60 +5,13 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Portfolio Starter',
-  siteDescription: 'A simple portfolio theme for Gridsome powered by Tailwind CSS v1',
-  siteUrl: 'https://gridsome-portfolio-starter.netlify.com',
+  siteName: 'Untereiner.com',
+  siteDescription: 'The website of Laurent, Thomas & Romain Untereiner.',
+  siteUrl: 'https://untereiner.com',
   plugins: [
     {
       use: 'gridsome-plugin-tailwindcss',
     },
-    // {
-    //   use: '@gridsome/vue-remark',
-    //   options: {
-    //     typeName: 'Documentation', // Required
-    //     baseDir: './docs', // Where .md files are located
-    //     pathPrefix: '/docs', // Add route prefix. Optional
-    //     template: './src/templates/Documentation.vue', // Optional
-    //     plugins: [
-    //       [ 'gridsome-plugin-remark-shiki', { theme: 'Material-Theme-Palenight', skipInline: true } ]
-    //   ],
-    //   }
-    // },
-    // {
-    //   use: '@gridsome/source-filesystem',
-    //   options: {
-    //     path: 'blog/**/*.md',
-    //     typeName: 'Post',
-    //     refs: {
-    //       tags: {
-    //         typeName: 'Tag',
-    //         create: true
-    //       }
-    //     }
-    //   }
-    // },
-    // {
-    //   use: 'gridsome-plugin-rss',
-    //   options: {
-    //     contentTypeName: 'Post',
-    //     feedOptions: {
-    //       title: 'Gridsome Portfolio Starter Blog',
-    //       feed_url: 'https://gridsome-portfolio-starter.netlify.com/rss.xml',
-    //       site_url: 'https://gridsome-portfolio-starter.netlify.com/'
-    //     },
-    //     feedItemOptions: node => ({
-    //       title: node.title,
-    //       description: node.summary,
-    //       url: 'https://gridsome-portfolio-starter.netlify.com' + node.path,
-    //       author: 'Andre Madarang',
-    //       date: node.date
-    //     }),
-    //     output: {
-    //       dir: './static',
-    //       name: 'rss.xml'
-    //     }
-    //   }
-    // },
     {
       use: '@gridsome/plugin-sitemap',
       options: {
@@ -66,9 +19,6 @@ module.exports = {
       }
     },
   ],
-  // templates: {
-  //   Tag: '/tag/:id'
-  // },
   transformers: {
     remark: {
       plugins: [
